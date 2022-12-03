15b4f67dde8c381b27aae2b9ba47b9867084797a
igordelesposti <igordelesposti@gmail.com>
2022-12-03 15:30:25 -0300

    feat: create the script to install nginx-web on the instance

--- main.tf
+++ main.tf
@@ -7,8 +7,11 @@ variable "region" {
   default = "us-east-1"
 }
 
+# Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_key_pair" "k8s-key" {
