47af07d7c48305ccaa4b9accb97d8a60ae9c2a6b
igordelesposti <igordelesposti@gmail.com>
2022-12-03 17:32:43 -0300

    feat: adding variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subnet_id_1" {
+  type    = string
+  default = "clicksign_public_subnet.id"
+}
+
+variable "subnet_id_2" {
+  type    = string
+  default = "clicksign_private_subnet.id"
+}
