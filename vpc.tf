381a89ae40af1bc51310147aec487bb2e6ad6af0
igordelesposti <igordelesposti@gmail.com>
2022-12-04 17:46:26 -0300

    fix: minor changes at variables and vpc private subnets

--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,9 @@ module "vpc" {
   name = "clicksign-vpc"
   cidr = "10.0.0.0/16"
 
-  azs            = ["us-east-1a"]
-  public_subnets = ["10.0.101.0/24"]
+  azs             = ["us-east-1a"]
+  private_subnets = ["10.0.0.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
