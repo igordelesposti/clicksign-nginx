6e1645aa253b6a5127bb9f1245dcc6e63555d63f
igordelesposti <igordelesposti@gmail.com>
2022-12-04 16:22:40 -0300

    feat: add main and provider to building ec2 with nginx

--- main.tf
+++ main.tf
@@ -1,136 +1,48 @@
-# Variables
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "key_name" {}
-variable "private_key_path" {}
-variable "region" {
-  default = "us-east-1"
-}
-provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
-resource "aws_iam_role" "eks-iam-role" {
-  name = "clicksign-eks-iam-role"
-
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-  {
-   "Effect": "Allow",
-   "Principal": {
-    "Service": "eks.amazonaws.com"
-   },
-   "Action": "sts:AssumeRole"
-  }
- ]
-}
-EOF
-
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-iam-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-iam-role.name
-}
-resource "aws_vpc" "clicksign_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "clicksign_vpc"
-  }
-}
-
-resource "aws_subnet" "clicksign_public_subnet" {
-  vpc_id     = aws_vpc.clicksign_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "clicksign_public_subnet"
-  }
-}
-
-resource "aws_subnet" "clicksign_private_subnet" {
-  vpc_id     = aws_vpc.clicksign_vpc.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "clicksign_public_subnet"
-  }
-}
-resource "aws_eks_cluster" "clicksign-eks" {
-  name     = "clicksign-cluster"
-  role_arn = aws_iam_role.eks-iam-role.arn
-
-  vpc_config {
-    subnet_ids = [var.subnet_id_1, var.subnet_id_2]
-  }
-
+resource "aws_instance" "nginx-web" {
+  tags = merge(
+    local.tags,
+    {
+      Name = "nginx-web"
+    }
+  )
+  instance_type = "t2.micro"
+  ami           = "ami-052efd3df9dad4825"
+  user_data     = file("userdata.tpl")
+  vpc_security_group_ids = [
+    aws_security_group.nginx-web_sg.id
+  ]
   depends_on = [
-    aws_iam_role.eks-iam-role,
+    module.vpc,
+    aws_security_group.nginx-web_sg
   ]
 }
 
-resource "aws_iam_role" "workernodes" {
-  name = "eks-node-group-role"
+# Security group
+resource "aws_security_group" "nginx-web_sg" {
+  tags = merge(
+    local.tags,
+    {
+      Name = "nginx-web_sg"
+    }
+  )
+  name        = "nginx-web_sg"
+  description = "allow http on port 80"
+  vpc_id      = module.vpc.default_vpc_id
+  depends_on = [
+    module.vpc,
+  ]
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.workernodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.workernodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
-  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role       = aws_iam_role.workernodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.workernodes.name
-}
-
-resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name    = aws_eks_cluster.clicksign-eks.name
-  node_group_name = "clicksign-workernodes"
-  node_role_arn   = aws_iam_role.workernodes.arn
-  subnet_ids      = [var.subnet_id_1, var.subnet_id_2]
-  instance_types  = ["t3.xlarge"]
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
-
